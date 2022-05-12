c83ef23aff403b36026deb77cb70fc7028db1902
nhy0052 <nhy0052@naver.com>
2022-05-12 23:04:52 +0900

    4.LOGIN - 4.0~4.1

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-alert("hi");
+
+//로그인 폼
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
+
+//로그인 버튼 글릭 이벤트
+function LoginBtnClick() {
+  const userName = loginInput.value;
+  console.log(userName);
+}
+
+loginButton.addEventListener("click", LoginBtnClick);
